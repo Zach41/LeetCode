6366202c250762a746ea0b6858769cde68622de6
Zach <zach_41@163.com>
2017-03-31 13:45:52 +0800

    344

---
+++ 344_reverse_string/solve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return "".join(list(reversed(s)))        
